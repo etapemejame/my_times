36b246940387a3511d8cf4b9e19824eeab365272
etapemejame <etapeemblek@yahoo.com>
2019-07-25 21:41:41 -0400

    Update testScript.sh

--- testScript.sh
+++ testScript.sh
@@ -2,5 +2,5 @@
 echo "This is an example of a script"
 echo "That is running in a build step"
 echo "This is a param $PASSED"
-NUM=(($NUMBERONE + $NUMBERTWO))
+NUM=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUM"
